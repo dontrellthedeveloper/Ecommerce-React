fb3779c00f65a435db53eb1700e35953341281a5
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-25 17:50:26 -0800

    added firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCGFgPF1Oje-bNsdJ4O9ghAh-GuSnsWnZM",
+    authDomain: "mern-ecommerce-59f8c.firebaseapp.com",
+    databaseURL: "https://mern-ecommerce-59f8c.firebaseio.com",
+    projectId: "mern-ecommerce-59f8c",
+    storageBucket: "mern-ecommerce-59f8c.appspot.com",
+    messagingSenderId: "314137418107",
+    appId: "1:314137418107:web:2f901461c59050704ac5de"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
