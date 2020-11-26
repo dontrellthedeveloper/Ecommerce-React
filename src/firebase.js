91d732c63856a358078e4176446e814e812466ff
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-25 19:21:43 -0800

    created an email link to sign into firebase from the register page

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from "firebase";
+import firebase from 'firebase/app';
+import "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCGFgPF1Oje-bNsdJ4O9ghAh-GuSnsWnZM",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// export
 export const auth = firebase.auth();
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
