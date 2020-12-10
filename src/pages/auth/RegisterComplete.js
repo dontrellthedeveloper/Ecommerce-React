34b2fa4952bdaed853fde33b25a28cff3f30281a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-09 21:47:20 -0800

    finished adding registration

--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -6,12 +6,47 @@ const RegisterComplete = ({ history }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    useState(() => {
+    useEffect(() => {
         setEmail(window.localStorage.getItem("emailForRegistration"));
+        // console.log(window.location.href);
+        // console.log(window.localStorage.getItem("emailForRegistration"));
     }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // validation
+        if (!email || !password) {
+            toast.error("Email and password is required");
+            return;
+        }
+
+        if (password.length < 6) {
+            toast.error("Password must be at least 6 characters long");
+            return;
+        }
+
+        try {
+            const result = await auth.signInWithEmailLink(
+                email,
+                window.location.href
+            );
+            //   console.log("RESULT", result);
+            if (result.user.emailVerified) {
+                // remove user email fom local storage
+                window.localStorage.removeItem("emailForRegistration");
+                // get user id token
+                let user = auth.currentUser;
+                await user.updatePassword(password);
+                const idTokenResult = await user.getIdTokenResult();
+                // redux store
+                console.log("user", user, "idTokenResult", idTokenResult);
+                // redirect
+                history.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
     };
 
     const completeRegistrationForm = () => (
