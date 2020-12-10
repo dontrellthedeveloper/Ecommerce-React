54d86c257ccf31b6e6014b440fe0a2593d183849
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-10 14:29:40 -0800

    added the ability to login with google using firebase

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { auth } from "../../firebase";
+import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 
 const Login = ({ history }) => {
-    const [email, setEmail] = useState("gqlreactnode@gmail.com");
-    const [password, setPassword] = useState("123456");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
 
     let dispatch = useDispatch();
@@ -35,6 +35,27 @@ const Login = ({ history }) => {
             toast.error(error.message);
             setLoading(false);
         }
+    };
+
+    const googleLogin = async () => {
+        auth
+            .signInWithPopup(googleAuthProvider)
+            .then(async (result) => {
+                const { user } = result;
+                const idTokenResult = await user.getIdTokenResult();
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+                history.push("/");
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error(err.message);
+            });
     };
 
     const loginForm = () => (
@@ -80,8 +101,24 @@ const Login = ({ history }) => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4>Login</h4>
+                    {loading ? (
+                        <h4 className="text-danger">Loading...</h4>
+                    ) : (
+                        <h4>Login</h4>
+                    )}
                     {loginForm()}
+
+                    <Button
+                        onClick={googleLogin}
+                        type="danger"
+                        className="mb-3"
+                        block
+                        shape="round"
+                        icon={<GoogleOutlined />}
+                        size="large"
+                    >
+                        Login with Google
+                    </Button>
                 </div>
             </div>
         </div>
