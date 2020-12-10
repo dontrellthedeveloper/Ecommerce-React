43790da2dabbd3c7ec372310c82a2687bfde10ed
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-10 00:30:27 -0800

    added redux to website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+// store
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-    //<React.StrictMode>
+    // <React.StrictMode>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
-        </BrowserRouter>,
+        </BrowserRouter>
+    </Provider>,
     // </React.StrictMode>,
     document.getElementById("root")
 );
