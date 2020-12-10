1df3b6f184b6666b204c5f1075a4d2e44f6163d2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-10 15:25:47 -0800

    added forgot password functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -44,6 +44,7 @@ const App = () => {
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/register/complete" component={RegisterComplete} />
+                <Route exact path="/forgot/password" component={ForgotPassword} />
             </Switch>
         </>
     );
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Login = ({ history }) => {
     const [email, setEmail] = useState("");
@@ -119,6 +120,10 @@ const Login = ({ history }) => {
                     >
                         Login with Google
                     </Button>
+
+                    <Link to="/forgot/password" className="float-right text-danger">
+                        Forgot Password
+                    </Link>
                 </div>
             </div>
         </div>
