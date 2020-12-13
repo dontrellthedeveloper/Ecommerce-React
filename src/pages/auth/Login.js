49ef6b0e4a2b55c8e91666cd80ad061d76f2b3ee
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-12 20:50:48 -0800

    Installed axios to connect client to server. Added ability to send auth token to backend

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -5,10 +5,23 @@ import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const createOrUpdateUser = async (authtoken) => {
+    return await axios.post(
+        `${process.env.REACT_APP_API}/create-or-update-user`,
+        {},
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
 
 const Login = ({ history }) => {
-    const [email, setEmail] = useState("gqlreactnode@gmail.com");
-    const [password, setPassword] = useState("123456");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
 
     const { user } = useSelector((state) => ({ ...state }));
@@ -29,14 +42,18 @@ const Login = ({ history }) => {
             const { user } = result;
             const idTokenResult = await user.getIdTokenResult();
 
-            dispatch({
-                type: "LOGGED_IN_USER",
-                payload: {
-                    email: user.email,
-                    token: idTokenResult.token,
-                },
-            });
-            history.push("/");
+            createOrUpdateUser(idTokenResult.token)
+                .then((res) => console.log("CREATE OR UPDATE RES", res))
+                .catch();
+
+            // dispatch({
+            //   type: "LOGGED_IN_USER",
+            //   payload: {
+            //     email: user.email,
+            //     token: idTokenResult.token,
+            //   },
+            // });
+            // history.push("/");
         } catch (error) {
             console.log(error);
             toast.error(error.message);
