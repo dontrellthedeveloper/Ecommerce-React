301ae955fee80683883318c5d434d7c634e1e712
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-16 09:55:47 -0800

    added the ability to render categories from the database and delete categories from the database

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -7,6 +7,8 @@ import {
     getCategories,
     removeCategory,
 } from "../../../functions/category";
+import { Link } from "react-router-dom";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const CategoryCreate = () => {
     const { user } = useSelector((state) => ({ ...state }));
@@ -38,6 +40,26 @@ const CategoryCreate = () => {
                 setLoading(false);
                 if (err.response.status === 400) toast.error(err.response.data);
             });
+    };
+
+    const handleRemove = async (slug) => {
+        // let answer = window.confirm("Delete?");
+        // console.log(answer, slug);
+        if (window.confirm("Delete?")) {
+            setLoading(true);
+            removeCategory(slug, user.token)
+                .then((res) => {
+                    setLoading(false);
+                    toast.error(`${res.data.name} deleted`);
+                    loadCategories();
+                })
+                .catch((err) => {
+                    if (err.response.status === 400) {
+                        setLoading(false);
+                        toast.error(err.response.data);
+                    }
+                });
+        }
     };
 
     const categoryForm = () => (
@@ -72,7 +94,22 @@ const CategoryCreate = () => {
                     )}
                     {categoryForm()}
                     <hr />
-                    {JSON.stringify(categories)}
+                    {categories.map((c) => (
+                        <div className="alert alert-secondary" key={c._id}>
+                            {c.name}
+                            <span
+                                onClick={() => handleRemove(c.slug)}
+                                className="btn btn-sm float-right"
+                            >
+                <DeleteOutlined className="text-danger" />
+              </span>
+                            <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
