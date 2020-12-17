1791fa2a02b1394e36d5381c32d3164520aa17aa
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-17 11:23:05 -0800

    added ability to save a category on the create product page

--- src/components/forms/ProductCreateForm.js
+++ src/components/forms/ProductCreateForm.js
@@ -101,6 +101,23 @@ const ProductCreateForm = ({ handleSubmit, handleChange, values }) => {
                 </select>
             </div>
 
+            <div className="form-group">
+                <label>Category</label>
+                <select
+                    name="category"
+                    className="form-control"
+                    onChange={handleChange}
+                >
+                    <option>Please select</option>
+                    {categories.length > 0 &&
+                    categories.map((c) => (
+                        <option key={c._id} value={c._id}>
+                            {c.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
             <button className="btn btn-outline-info">Save</button>
         </form>
     );
