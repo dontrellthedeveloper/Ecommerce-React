7d23bbbd6f02711cb221339870ce8842c3c9e95f
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-17 01:01:08 -0800

    added create product page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import CategoryCreate from "./pages/admin/category/CategoryCreate";
 import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
 import SubCreate from "./pages/admin/sub/SubCreate";
 import SubUpdate from "./pages/admin/sub/SubUpdate";
+import ProductCreate from "./pages/admin/product/ProductCreate";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -71,6 +72,7 @@ const App = () => {
                 <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate}/>
                 <AdminRoute exact path="/admin/sub" component={SubCreate} />
                 <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
+                <AdminRoute exact path="/admin/product" component={ProductCreate} />
             </Switch>
         </>
     );
---
+++ src/pages/admin/product/ProductCreate.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { createProduct } from "../../../functions/product";
+
+const ProductCreate = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <AdminNav />
+                </div>
+
+                <div className="col-md-10">product create form</div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCreate;
