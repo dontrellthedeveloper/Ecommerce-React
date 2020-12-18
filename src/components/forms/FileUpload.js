a956f5da41232388fa6eaf4b999be307a26fe151
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-18 10:49:58 -0800

    added the ability to create a product with multiple images from the admin dashboard

--- src/components/forms/FileUpload.js
+++ src/components/forms/FileUpload.js
@@ -2,7 +2,7 @@ import React from "react";
 import Resizer from "react-image-file-resizer";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import { Avatar } from "antd";
+import { Avatar, Badge } from "antd";
 
 const FileUpload = ({ values, setValues, setLoading }) => {
     const { user } = useSelector((state) => ({ ...state }));
@@ -55,17 +55,51 @@ const FileUpload = ({ values, setValues, setLoading }) => {
         // set url to images[] in the parent component state - ProductCreate
     };
 
+    const handleImageRemove = (public_id) => {
+        setLoading(true);
+        // console.log("remove image", public_id);
+        axios
+            .post(
+                `${process.env.REACT_APP_API}/removeimage`,
+                { public_id },
+                {
+                    headers: {
+                        authtoken: user ? user.token : "",
+                    },
+                }
+            )
+            .then((res) => {
+                setLoading(false);
+                const { images } = values;
+                let filteredImages = images.filter((item) => {
+                    return item.public_id !== public_id;
+                });
+                setValues({ ...values, images: filteredImages });
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            });
+    };
+
     return (
         <>
             <div className="row">
                 {values.images &&
                 values.images.map((image) => (
-                    <Avatar
+                    <Badge
+                        count="X"
                         key={image.public_id}
-                        src={image.url}
-                        size={100}
-                        className="m-3"
-                    />
+                        onClick={() => handleImageRemove(image.public_id)}
+                        style={{ cursor: "pointer" }}
+                    >
+                        <Avatar
+                            src={image.url}
+                            size={100}
+                            shape="square"
+                            className="ml-3"
+                        />
+                    </Badge>
                 ))}
             </div>
             <div className="row">
--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -6,6 +6,7 @@ import { createProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const initialState = {
     title: "Macbook Pro",
@@ -16,7 +17,23 @@ const initialState = {
     subs: [],
     shipping: "Yes",
     quantity: "50",
-    images: [],
+    images: [
+        // {
+        //   public_id: "jwrzeubemmypod99e8lz",
+        //   url:
+        //     "https://res.cloudinary.com/dcqjrwaoi/image/upload/v1599480909/jwrzeubemmypod99e8lz.jpg",
+        // },
+        // {
+        //   public_id: "j7uerlvhog1eic0oyize",
+        //   url:
+        //     "https://res.cloudinary.com/dcqjrwaoi/image/upload/v1599480912/j7uerlvhog1eic0oyize.jpg",
+        // },
+        // {
+        //   public_id: "ho6wnp7sugyemnmtoogf",
+        //   url:
+        //     "https://res.cloudinary.com/dcqjrwaoi/image/upload/v1599480913/ho6wnp7sugyemnmtoogf.jpg",
+        // },
+    ],
     colors: ["Black", "Brown", "Silver", "White", "Blue"],
     brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
     color: "White",
@@ -78,10 +95,14 @@ const ProductCreate = () => {
                 </div>
 
                 <div className="col-md-10">
-                    <h4>Product create</h4>
+                    {loading ? (
+                        <LoadingOutlined className="text-danger h1" />
+                    ) : (
+                        <h4>Product create</h4>
+                    )}
                     <hr />
 
-                    {JSON.stringify(values.images)}
+                    {/* {JSON.stringify(values.images)} */}
 
                     <div className="p-3">
                         <FileUpload
