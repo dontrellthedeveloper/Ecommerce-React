e6f27f57776c603df5186bceb2ad80f4a1ca5fd6
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 13:25:51 -0800

    added default image if products don't have an image

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card } from "antd";
+import laptop from "../../images/laptop.png";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
@@ -11,13 +13,20 @@ const AdminProductCard = ({ product }) => {
         <Card
             cover={
                 <img
-                    src={images && images.length ? images[0].url : ""}
+                    src={images && images.length ? images[0].url : laptop}
                     style={{ height: "150px", objectFit: "cover" }}
                     className="p-1"
                 />
             }
+            actions={[
+                <EditOutlined className="text-warning" />,
+                <DeleteOutlined className="text-danger" />,
+            ]}
         >
-            <Meta title={title} description={description} />
+            <Meta
+                title={title}
+                description={`${description && description.substring(0, 40)}...`}
+            />
         </Card>
     );
 };
