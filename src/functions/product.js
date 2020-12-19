d32da817af1333626449bd92220a38bcc0522f70
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 14:45:05 -0800

    added the ability to get product on the update page

--- src/functions/product.js
+++ src/functions/product.js
@@ -17,3 +17,6 @@ export const removeProduct = async (slug, authtoken) =>
             authtoken,
         },
     });
+
+export const getProduct = async (slug) =>
+    await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,16 +2,45 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
+const initialState = {
+    title: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    subs: [],
+    shipping: "",
+    quantity: "",
+    images: [],
+    colors: ["Black", "Brown", "Silver", "White", "Blue"],
+    brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+    color: "",
+    brand: "",
+};
+
 const ProductUpdate = ({ match }) => {
+    // state
+    const [values, setValues] = useState(initialState);
+
     const { user } = useSelector((state) => ({ ...state }));
     // router
     const { slug } = match.params;
+
+    useEffect(() => {
+        loadProduct();
+    }, []);
+
+    const loadProduct = () => {
+        getProduct(slug).then((p) => {
+            // console.log("single product", p);
+            setValues({ ...values, ...p.data });
+        });
+    };
 
     return (
         <div className="container-fluid">
@@ -22,7 +51,7 @@ const ProductUpdate = ({ match }) => {
 
                 <div className="col-md-10">
                     <h4>Product update</h4>
-                    {JSON.stringify(slug)}
+                    {JSON.stringify(values)}
                     <hr />
                 </div>
             </div>
