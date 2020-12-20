d2ebea78719ec5c9d33b5fe40706b35060f8018b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 20:35:33 -0800

    added the ability to update a product from the admin dashboard

--- src/functions/product.js
+++ src/functions/product.js
@@ -20,3 +20,10 @@ export const removeProduct = async (slug, authtoken) =>
 
 export const getProduct = async (slug) =>
     await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
+
+export const updateProduct = async (slug, product, authtoken) =>
+    await axios.put(`${process.env.REACT_APP_API}/product/${slug}`, product, {
+        headers: {
+            authtoken,
+        },
+    });
