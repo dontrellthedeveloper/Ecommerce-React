88a3971c45b3ac9d79bcdb5ac37101090596b457
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 23:24:19 -0800

    added new arrivals component

--- src/functions/product.js
+++ src/functions/product.js
@@ -27,3 +27,11 @@ export const updateProduct = async (slug, product, authtoken) =>
             authtoken,
         },
     });
+
+
+export const getProducts = async (sort, order, limit) =>
+    await axios.post(`${process.env.REACT_APP_API}/products`, {
+        sort,
+        order,
+        limit,
+    });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { getProductsByCount } from "../functions/product";
-import ProductCard from "../components/cards/ProductCard";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import LoadingCard from "../components/cards/LoadingCard";
+import NewArrivals from "../components/home/NewArrivals";
+
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        loadAllProducts();
-    }, []);
-
-    const loadAllProducts = () => {
-        setLoading(true);
-        getProductsByCount(3).then((res) => {
-            setProducts(res.data);
-            setLoading(false);
-        });
-    };
-
     return (
         <>
             <div className="jumbotron text-danger h1 font-weight-bold text-center">
                 <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
             </div>
 
-            <div className="container">
-                {loading ? (
-                    <LoadingCard count={3} />
-                ) : (
-                    <div className="row">
-                        {products.map((product) => (
-                            <div key={product._id} className="col-md-4">
-                                <ProductCard product={product} />
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+                New Arrivals
+            </h4>
+            <NewArrivals />
+
+
+
+
+            <br />
         </>
     );
 };
