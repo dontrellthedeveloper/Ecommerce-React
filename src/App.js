c0513b6d6055fb0e220cb432acd1b8915deec8eb
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-20 11:32:35 -0800

    added view products page

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import SubUpdate from "./pages/admin/sub/SubUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
 import ProductUpdate from "./pages/admin/product/ProductUpdate";
+import Product from "./pages/Product";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -86,6 +87,7 @@ const App = () => {
                     path="/admin/product/:slug"
                     component={ProductUpdate}
                 />
+                <Route exact path="/product/:slug" component={Product} />
             </Switch>
         </>
     );
