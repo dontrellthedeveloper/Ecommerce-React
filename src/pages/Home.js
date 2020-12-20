b926ad8f2bfb5ebd5a233b80001287bc1e377cba
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 23:29:48 -0800

    added best sellers component to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
-
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
     return (
@@ -15,9 +15,12 @@ const Home = () => {
             </h4>
             <NewArrivals />
 
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+                Best Sellers
+            </h4>
+            <BestSellers />
 
-
-
+            <br />
             <br />
         </>
     );
