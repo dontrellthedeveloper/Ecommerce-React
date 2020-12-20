902f996959bfaef15ffc050bb9b74d8fd2f84e3c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 21:20:44 -0800

    added product card component

---
+++ src/components/cards/ProductCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ProductCard = ({ product }) => <div>{product.title}</div>;
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getProductsByCount } from "../functions/product";
+import ProductCard from "../components/cards/ProductCard";
 
 const Home = () => {
     const [products, setProducts] = useState([]);
@@ -10,18 +11,30 @@ const Home = () => {
     }, []);
 
     const loadAllProducts = () => {
+        setLoading(true);
         getProductsByCount(3).then((res) => {
             setProducts(res.data);
+            setLoading(false);
         });
     };
 
     return (
-        <div>
-            <p>react home</p>
-            {JSON.stringify(products)}
-        </div>
+        <>
+            <div className="jumbotron">
+                {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    {products.map((product) => (
+                        <div key={product._id} className="col-md-4">
+                            <ProductCard product={product} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 
 export default Home;
-
