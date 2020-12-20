07c820a8ac30b178092b7b1281ec22b512f9d110
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 21:41:31 -0800

    added typewriter effect on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getProductsByCount } from "../functions/product";
 import ProductCard from "../components/cards/ProductCard";
+import Jumbotron from "../components/cards/Jumbotron";
 
 const Home = () => {
     const [products, setProducts] = useState([]);
@@ -20,8 +21,8 @@ const Home = () => {
 
     return (
         <>
-            <div className="jumbotron">
-                {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+            <div className="jumbotron text-danger h1 font-weight-bold text-center">
+                <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
             </div>
 
             <div className="container">
