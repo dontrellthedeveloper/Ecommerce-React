39081c53d56b0a897d3cd2da82ba4977221cd356
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-20 21:42:52 -0800

    added category list on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
 import BestSellers from "../components/home/BestSellers";
+import CategoryList from "../components/category/CategoryList";
 
 const Home = () => {
     return (
@@ -19,6 +20,11 @@ const Home = () => {
                 Best Sellers
             </h4>
             <BestSellers />
+
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+                Categories
+            </h4>
+            <CategoryList />
 
             <br />
             <br />
