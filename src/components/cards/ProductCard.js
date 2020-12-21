a8f821735865afa737347b5b5c16c280934b7a9a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-20 21:24:07 -0800

    show average product rating on homepage

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -3,6 +3,7 @@ import { Card } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
+import { showAverage } from "../../functions/rating";
 
 const { Meta } = Card;
 
@@ -10,28 +11,36 @@ const ProductCard = ({ product }) => {
     // destructure
     const { images, title, description, slug } = product;
     return (
-        <Card
-            cover={
-                <img
-                    src={images && images.length ? images[0].url : laptop}
-                    style={{ height: "150px", objectFit: "cover" }}
-                    className="p-1"
+        <>
+            {product && product.ratings && product.ratings.length > 0 ? (
+                showAverage(product)
+            ) : (
+                <div className="text-center pt-1 pb-3">No rating yet</div>
+            )}
+
+            <Card
+                cover={
+                    <img
+                        src={images && images.length ? images[0].url : laptop}
+                        style={{ height: "150px", objectFit: "cover" }}
+                        className="p-1"
+                    />
+                }
+                actions={[
+                    <Link to={`/product/${slug}`}>
+                        <EyeOutlined className="text-warning" /> <br /> View Product
+                    </Link>,
+                    <>
+                        <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
+                    </>,
+                ]}
+            >
+                <Meta
+                    title={title}
+                    description={`${description && description.substring(0, 40)}...`}
                 />
-            }
-            actions={[
-                <Link to={`/product/${slug}`}>
-                    <EyeOutlined className="text-warning" /> <br /> View Product
-                </Link>,
-                <>
-                    <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-                </>,
-            ]}
-        >
-            <Meta
-                title={title}
-                description={`${description && description.substring(0, 40)}...`}
-            />
-        </Card>
+            </Card>
+        </>
     );
 };
 
