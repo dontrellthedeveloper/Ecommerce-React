1b5c3bb37ec5ee44fa976c56670fdc7cc435a617
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-20 22:11:04 -0800

    finished adding categories page with products

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -13,13 +13,39 @@ const CategoryHome = ({ match }) => {
 
     useEffect(() => {
         setLoading(true);
-        getCategory(slug).then((c) => {
-            console.log(JSON.stringify(c.data, null, 4));
-            setCategory(c.data);
+        getCategory(slug).then((res) => {
+            console.log(JSON.stringify(res.data, null, 4));
+            setCategory(res.data.category);
+            setProducts(res.data.products);
+            setLoading(false);
         });
     }, []);
 
-    return <p>{slug}</p>;
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    {loading ? (
+                        <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+                            Loading...
+                        </h4>
+                    ) : (
+                        <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+                            {products.length} Products in "{category.name}" category
+                        </h4>
+                    )}
+                </div>
+            </div>
+
+            <div className="row">
+                {products.map((p) => (
+                    <div className="col" key={p._id}>
+                        <ProductCard product={p} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default CategoryHome;
