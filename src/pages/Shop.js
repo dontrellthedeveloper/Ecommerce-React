64592f087537f9c6a6ee1a11f60275a213fc7744
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-21 21:34:06 -0800

    added the ability to filter by categories in the front end

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,10 +3,11 @@ import {
     getProductsByCount,
     fetchProductsByFilter,
 } from "../functions/product";
+import { getCategories } from "../functions/category";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
-import { Menu, Slider } from "antd";
-import { DollarOutlined } from "@ant-design/icons";
+import { Menu, Slider, Checkbox } from "antd";
+import { DollarOutlined, DownSquareOutlined } from "@ant-design/icons";
 
 const { SubMenu, ItemGroup } = Menu;
 
@@ -15,6 +16,8 @@ const Shop = () => {
     const [loading, setLoading] = useState(false);
     const [price, setPrice] = useState([0, 0]);
     const [ok, setOk] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const [categoryIds, setCategoryIds] = useState([]);
 
     let dispatch = useDispatch();
     let { search } = useSelector((state) => ({ ...state }));
@@ -22,6 +25,8 @@ const Shop = () => {
 
     useEffect(() => {
         loadAllProducts();
+        // fetch categories
+        getCategories().then((res) => setCategories(res.data));
     }, []);
 
     const fetchProducts = (arg) => {
@@ -57,10 +62,54 @@ const Shop = () => {
             type: "SEARCH_QUERY",
             payload: { text: "" },
         });
+        setCategoryIds([]);
         setPrice(value);
         setTimeout(() => {
             setOk(!ok);
         }, 300);
+    };
+
+    // 4. load products based on category
+    // show categories in a list of checkbox
+    const showCategories = () =>
+        categories.map((c) => (
+            <div key={c._id}>
+                <Checkbox
+                    onChange={handleCheck}
+                    className="pb-2 pl-4 pr-4"
+                    value={c._id}
+                    name="category"
+                    checked={categoryIds.includes(c._id)}
+                >
+                    {c.name}
+                </Checkbox>
+                <br />
+            </div>
+        ));
+
+    // handle check for categories
+    const handleCheck = (e) => {
+        dispatch({
+            type: "SEARCH_QUERY",
+            payload: { text: "" },
+        });
+        setPrice([0, 0]);
+        // console.log(e.target.value);
+        let inTheState = [...categoryIds];
+        let justChecked = e.target.value;
+        let foundInTheState = inTheState.indexOf(justChecked); // index or -1
+
+        // indexOf method ?? if not found returns -1 else return index [1,2,3,4,5]
+        if (foundInTheState === -1) {
+            inTheState.push(justChecked);
+        } else {
+            // if found pull out one item from index
+            inTheState.splice(foundInTheState, 1);
+        }
+
+        setCategoryIds(inTheState);
+        // console.log(inTheState);
+        fetchProducts({ category: inTheState });
     };
 
     return (
@@ -71,6 +120,7 @@ const Shop = () => {
                     <hr />
 
                     <Menu defaultOpenKeys={["1", "2"]} mode="inline">
+                        {/* price */}
                         <SubMenu
                             key="1"
                             title={
@@ -89,6 +139,18 @@ const Shop = () => {
                                     max="4999"
                                 />
                             </div>
+                        </SubMenu>
+
+                        {/* category */}
+                        <SubMenu
+                            key="2"
+                            title={
+                                <span className="h6">
+                  <DownSquareOutlined /> Categories
+                </span>
+                            }
+                        >
+                            <div style={{ maringTop: "-10px" }}>{showCategories()}</div>
                         </SubMenu>
                     </Menu>
                 </div>
