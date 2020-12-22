d73f0607c488a70eb24445526624c17999ad75af
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-21 22:31:08 -0800

    add product to local storage when you click add to Cart button

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -4,10 +4,32 @@ import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
 import { showAverage } from "../../functions/rating";
+import _ from "lodash";
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+    const handleAddToCart = () => {
+        // create cart array
+        let cart = [];
+        if (typeof window !== "undefined") {
+            // if cart is in local storage GET it
+            if (localStorage.getItem("cart")) {
+                cart = JSON.parse(localStorage.getItem("cart"));
+            }
+            // push new product to cart
+            cart.push({
+                ...product,
+                count: 1,
+            });
+            // remove duplicates
+            let unique = _.uniqWith(cart, _.isEqual);
+            // save to local storage
+            // console.log('unique', unique)
+            localStorage.setItem("cart", JSON.stringify(unique));
+        }
+    };
+
     // destructure
     const { images, title, description, slug, price } = product;
     return (
@@ -30,9 +52,9 @@ const ProductCard = ({ product }) => {
                     <Link to={`/product/${slug}`}>
                         <EyeOutlined className="text-warning" /> <br /> View Product
                     </Link>,
-                    <>
+                    <a onClick={handleAddToCart}>
                         <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-                    </>,
+                    </a>,
                 ]}
             >
                 <Meta
