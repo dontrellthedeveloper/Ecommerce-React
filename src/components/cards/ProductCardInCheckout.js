85784a132e8f2c48b950aaa5f45994373afd5c62
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-21 23:25:50 -0800

    added react-modal-image and the ability to pick a color in the cart page

--- src/components/cards/ProductCardInCheckout.js
+++ src/components/cards/ProductCardInCheckout.js
@@ -1,14 +1,71 @@
 import React from "react";
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop.png";
+import { useDispatch } from "react-redux";
 
 const ProductCardInCheckout = ({ p }) => {
+    const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+    let dispatch = useDispatch();
+
+    const handleColorChange = (e) => {
+        console.log("color changed", e.target.value);
+
+        let cart = [];
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem("cart")) {
+                cart = JSON.parse(localStorage.getItem("cart"));
+            }
+
+            cart.map((product, i) => {
+                if (product._id === p._id) {
+                    cart[i].color = e.target.value;
+                }
+            });
+
+            //  console.log('cart udpate color', cart)
+            localStorage.setItem("cart", JSON.stringify(cart));
+            dispatch({
+                type: "ADD_TO_CART",
+                payload: cart,
+            });
+        }
+    };
+
     return (
         <tbody>
         <tr>
-            <td>Image</td>
+            <td>
+                <div style={{ width: "100px", height: "auto" }}>
+                    {p.images.length ? (
+                        <ModalImage small={p.images[0].url} large={p.images[0].url} />
+                    ) : (
+                        <ModalImage small={laptop} large={laptop} />
+                    )}
+                </div>
+            </td>
             <td>{p.title}</td>
             <td>${p.price}</td>
             <td>{p.brand}</td>
-            <td>{p.color}</td>
+            <td>
+                <select
+                    onChange={handleColorChange}
+                    name="color"
+                    className="form-control"
+                >
+                    {p.color ? (
+                        <option value={p.color}>{p.color}</option>
+                    ) : (
+                        <option>Select</option>
+                    )}
+                    {colors
+                        .filter((c) => c !== p.color)
+                        .map((c) => (
+                            <option key={c} value={c}>
+                                {c}
+                            </option>
+                        ))}
+                </select>
+            </td>
             <td>{p.count}</td>
             <td>Shipping Icon</td>
             <td>Delete Icon</td>
@@ -18,3 +75,4 @@ const ProductCardInCheckout = ({ p }) => {
 };
 
 export default ProductCardInCheckout;
+
