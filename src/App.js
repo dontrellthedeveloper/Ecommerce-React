b226d13b7643be4ead79cef7606aeebdf1582c9c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-21 23:36:00 -0800

    added side drawer component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
+import SideDrawer from "./components/drawer/SideDrawer";
+
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import History from "./pages/user/History";
@@ -65,6 +67,7 @@ const App = () => {
     return (
         <>
             <Header />
+            <SideDrawer />
             <ToastContainer />
             <Switch>
                 <Route exact path="/" component={Home} />
