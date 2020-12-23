75ae55fa333e3231237251bdb24a7d0d552ee6d0
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-23 13:59:18 -0800

    show purchased orders in a card

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -19,13 +19,33 @@ const History = () => {
             setOrders(res.data);
         });
 
+    const showOrderInTable = (order) => <p>each order and it's products</p>;
+
+    const showEachOrders = () =>
+        orders.map((order, i) => (
+            <div key={i} className="m-5 p-3 card">
+                <p>show payment info</p>
+                {showOrderInTable(order)}
+                <div className="row">
+                    <div className="col">
+                        <p>PDF download</p>
+                    </div>
+                </div>
+            </div>
+        ));
+
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-2">
                     <UserNav />
                 </div>
-                <div className="col">user history page</div>
+                <div className="col text-center">
+                    <h4>
+                        {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+                    </h4>
+                    {showEachOrders()}
+                </div>
             </div>
         </div>
     );
