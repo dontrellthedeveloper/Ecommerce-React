789fc59d90e73312374610c0f1b0b34a84522f29
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-23 11:42:34 -0800

    added create order and empty cart frontend

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { Card } from "antd";
 import { DollarOutlined, CheckOutlined, SwapOutlined } from "@ant-design/icons";
 import Laptop from "../images/laptop.png";
+import { createOrder, emptyUserCart } from "../functions/user";
 
 const StripeCheckout = ({ history }) => {
     const dispatch = useDispatch();
@@ -54,6 +55,24 @@ const StripeCheckout = ({ history }) => {
         } else {
             // here you get result after successful payment
             // create order and save in database for admin to process
+            createOrder(payload, user.token).then((res) => {
+                if (res.data.ok) {
+                    // empty cart from local storage
+                    if (typeof window !== "undefined") localStorage.removeItem("cart");
+                    // empty cart from redux
+                    dispatch({
+                        type: "ADD_TO_CART",
+                        payload: [],
+                    });
+                    // reset coupon to false
+                    dispatch({
+                        type: "COUPON_APPLIED",
+                        payload: false,
+                    });
+                    // empty cart from database
+                    emptyUserCart(user.token);
+                }
+            });
             // empty user cart from redux store and local storage
             console.log(JSON.stringify(payload, null, 4));
             setError(null);
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -216,6 +216,7 @@ const Shop = () => {
     const showBrands = () =>
         brands.map((b) => (
             <Radio
+                key={b}
                 value={b}
                 name={b}
                 checked={b === brand}
@@ -245,6 +246,7 @@ const Shop = () => {
     const showColors = () =>
         colors.map((c) => (
             <Radio
+                key={c}
                 value={c}
                 name={c}
                 checked={c === color}
