333b35f2d9a8f36e5e4b0c99cb9622c30ec164c6
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-22 19:52:58 -0800

    added get user cart in the front end

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserCart } from "../functions/user";
 
 const Checkout = () => {
+    const [products, setProducts] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        getUserCart(user.token).then((res) => {
+            console.log("user cart res", JSON.stringify(res.data, null, 4));
+            setProducts(res.data.products);
+            setTotal(res.data.cartTotal);
+        });
+    }, []);
+
     const saveAddressToDb = () => {
         //
     };
@@ -23,6 +39,8 @@ const Checkout = () => {
 
             <div className="col-md-6">
                 <h4>Order Summary</h4>
+                <h1>{total}</h1>
+                {JSON.stringify(products)}
                 <hr />
                 <p>Products x</p>
                 <hr />
