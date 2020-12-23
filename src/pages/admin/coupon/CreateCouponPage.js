341e6912af88d6aa4326f433940dc843251a8c50
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-22 21:17:13 -0800

    added ability to list and delete coupons in the admin panel

--- src/pages/admin/coupon/CreateCouponPage.js
+++ src/pages/admin/coupon/CreateCouponPage.js
@@ -16,9 +16,16 @@ const CreateCouponPage = () => {
     const [expiry, setExpiry] = useState("");
     const [discount, setDiscount] = useState("");
     const [loading, setLoading] = useState("");
+    const [coupons, setCoupons] = useState([]);
 
     // redux
     const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        loadAllCoupons();
+    }, []);
+
+    const loadAllCoupons = () => getCoupons().then((res) => setCoupons(res.data));
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -27,12 +34,26 @@ const CreateCouponPage = () => {
         createCoupon({ name, expiry, discount }, user.token)
             .then((res) => {
                 setLoading(false);
+                loadAllCoupons(); // load all coupons
                 setName("");
                 setDiscount("");
                 setExpiry("");
                 toast.success(`"${res.data.name}" is created`);
             })
             .catch((err) => console.log("create coupon err", err));
+    };
+
+    const handleRemove = (couponId) => {
+        if (window.confirm("Delete?")) {
+            setLoading(true);
+            removeCoupon(couponId, user.token)
+                .then((res) => {
+                    loadAllCoupons(); // load all coupons
+                    setLoading(false);
+                    toast.error(`Coupon "${res.data.name}" deleted`);
+                })
+                .catch((err) => console.log(err));
+        }
     };
 
     return (
@@ -42,7 +63,11 @@ const CreateCouponPage = () => {
                     <AdminNav />
                 </div>
                 <div className="col-md-10">
-                    <h4>Coupon</h4>
+                    {loading ? (
+                        <h4 className="text-danger">Loading...</h4>
+                    ) : (
+                        <h4>Coupon</h4>
+                    )}
 
                     <form onSubmit={handleSubmit}>
                         <div className="form-group">
@@ -73,7 +98,7 @@ const CreateCouponPage = () => {
                             <br />
                             <DatePicker
                                 className="form-control"
-                                selected={new Date()}
+                                selected={expiry}
                                 value={expiry}
                                 onChange={(date) => setExpiry(date)}
                                 required
@@ -82,6 +107,37 @@ const CreateCouponPage = () => {
 
                         <button className="btn btn-outline-primary">Save</button>
                     </form>
+
+                    <br />
+
+                    <h4>{coupons.length} Coupons</h4>
+
+                    <table className="table table-bordered">
+                        <thead className="thead-light">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Expiry</th>
+                            <th scope="col">Discount</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        {coupons.map((c) => (
+                            <tr key={c._id}>
+                                <td>{c.name}</td>
+                                <td>{new Date(c.expiry).toLocaleDateString()}</td>
+                                <td>{c.discount}%</td>
+                                <td>
+                                    <DeleteOutlined
+                                        onClick={() => handleRemove(c._id)}
+                                        className="text-danger pointer"
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
