e52a2b8fa81dbd7b490a5077f10dd89eec58f918
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-22 20:27:38 -0800

    added ability to save address on checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { getUserCart, emptyUserCart, saveUserAddress } from "../functions/user";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Checkout = () => {
     const [products, setProducts] = useState([]);
     const [total, setTotal] = useState(0);
+    const [address, setAddress] = useState("");
+    const [addressSaved, setAddressSaved] = useState(false);
 
     const dispatch = useDispatch();
     const { user } = useSelector((state) => ({ ...state }));
@@ -37,7 +41,13 @@ const Checkout = () => {
     };
 
     const saveAddressToDb = () => {
-        //
+        // console.log(address);
+        saveUserAddress(user.token, address).then((res) => {
+            if (res.data.ok) {
+                setAddressSaved(true);
+                toast.success("Address saved");
+            }
+        });
     };
 
     return (
@@ -46,7 +56,7 @@ const Checkout = () => {
                 <h4>Delivery Address</h4>
                 <br />
                 <br />
-                textarea
+                <ReactQuill theme="snow" value={address} onChange={setAddress} />
                 <button className="btn btn-primary mt-2" onClick={saveAddressToDb}>
                     Save
                 </button>
@@ -74,7 +84,12 @@ const Checkout = () => {
 
                 <div className="row">
                     <div className="col-md-6">
-                        <button className="btn btn-primary">Place Order</button>
+                        <button
+                            className="btn btn-primary"
+                            disabled={!addressSaved || !products.length}
+                        >
+                            Place Order
+                        </button>
                     </div>
 
                     <div className="col-md-6">
