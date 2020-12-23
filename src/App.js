968906300115c07e9b163335f024e992667b4193
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-22 20:45:07 -0800

    added create coupon page

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ import SubHome from "./pages/sub/SubHome";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import CreateCouponPage from "./pages/admin/coupon/CreateCouponPage";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -101,6 +102,7 @@ const App = () => {
                 <Route exact path="/shop" component={Shop} />
                 <Route exact path="/cart" component={Cart} />
                 <UserRoute exact path="/checkout" component={Checkout} />
+                <AdminRoute exact path="/admin/coupon" component={CreateCouponPage} />
             </Switch>
         </>
     );
