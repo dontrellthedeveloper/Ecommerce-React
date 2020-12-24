6a09a689418827f7990f0e053d7533137ef88bc0
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-23 18:26:24 -0800

    added Cash on Delivery reducer

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -24,6 +24,20 @@ const Cart = ({ history }) => {
             .catch((err) => console.log("cart save err", err));
     };
 
+    const saveCashOrderToDb = () => {
+        // console.log("cart", JSON.stringify(cart, null, 4));
+        dispatch({
+            type: "COD",
+            payload: true,
+        });
+        userCart(cart, user.token)
+            .then((res) => {
+                console.log("CART POST RES", res);
+                if (res.data.ok) history.push("/checkout");
+            })
+            .catch((err) => console.log("cart save err", err));
+    };
+
     const showCartItems = () => (
         <table className="table table-bordered">
             <thead className="thead-light">
@@ -74,13 +88,23 @@ const Cart = ({ history }) => {
                     Total: <b>${getTotal()}</b>
                     <hr />
                     {user ? (
-                        <button
-                            onClick={saveOrderToDb}
-                            className="btn btn-sm btn-primary mt-2"
-                            disabled={!cart.length}
-                        >
-                            Proceed to Checkout
-                        </button>
+                        <>
+                            <button
+                                onClick={saveOrderToDb}
+                                className="btn btn-sm btn-primary mt-2"
+                                disabled={!cart.length}
+                            >
+                                Proceed to Checkout
+                            </button>
+                            <br />
+                            <button
+                                onClick={saveCashOrderToDb}
+                                className="btn btn-sm btn-warning mt-2"
+                                disabled={!cart.length}
+                            >
+                                Pay Cash on Delivery
+                            </button>
+                        </>
                     ) : (
                         <button className="btn btn-sm btn-primary mt-2">
                             <Link
