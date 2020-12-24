7faaa3191f3405cc5a53b3928a5f686d93c21a19
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-23 16:56:02 -0800

    added pdf view and download links

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -5,6 +5,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { toast } from "react-toastify";
 import ShowPaymentInfo from "../../components/cards/ShowPaymentInfo";
+import {
+    Document,
+    Page,
+    Text,
+    View,
+    StyleSheet,
+    PDFDownloadLink,
+    PDFViewer,
+} from "@react-pdf/renderer";
 
 const History = () => {
     const [orders, setOrders] = useState([]);
@@ -56,15 +65,32 @@ const History = () => {
         </table>
     );
 
+    const showDownloadLink = (order) => (
+        <PDFDownloadLink
+            document={
+                <Document>
+                    <Page size="A4">
+                        <View>
+                            <Text>Section #1</Text>
+                            <Text>Section #2</Text>
+                        </View>
+                    </Page>
+                </Document>
+            }
+            fileName="invoice.pdf"
+            className="btn btn-sm btn-block btn-outline-primary"
+        >
+            Download PDF
+        </PDFDownloadLink>
+    );
+
     const showEachOrders = () =>
         orders.map((order, i) => (
             <div key={i} className="m-5 p-3 card">
                 <ShowPaymentInfo order={order} />
                 {showOrderInTable(order)}
                 <div className="row">
-                    <div className="col">
-                        <p>PDF download</p>
-                    </div>
+                    <div className="col">{showDownloadLink(order)}</div>
                 </div>
             </div>
         ));
