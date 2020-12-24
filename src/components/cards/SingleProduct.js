7884715e3e0f45046ea8f9c469413f706d460ebd
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-23 18:16:20 -0800

    added wishlist frontend

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -11,6 +11,9 @@ import RatingModal from "../modal/RatingModal";
 import { showAverage } from "../../functions/rating";
 import _ from "lodash";
 import { useSelector, useDispatch } from "react-redux";
+import { addToWishlist } from "../../functions/user";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 const { TabPane } = Tabs;
 
@@ -21,6 +24,8 @@ const SingleProduct = ({ product, onStarClick, star }) => {
     // redux
     const { user, cart } = useSelector((state) => ({ ...state }));
     const dispatch = useDispatch();
+    // router
+    let history = useHistory();
 
     const { title, images, description, _id } = product;
 
@@ -58,6 +63,15 @@ const SingleProduct = ({ product, onStarClick, star }) => {
         }
     };
 
+    const handleAddToWishlist = (e) => {
+        e.preventDefault();
+        addToWishlist(product._id, user.token).then((res) => {
+            console.log("ADDED TO WISHLIST", res.data);
+            toast.success("Added to wishlist");
+            history.push("/user/wishlist");
+        });
+    };
+
     return (
         <>
             <div className="col-md-7">
@@ -90,15 +104,16 @@ const SingleProduct = ({ product, onStarClick, star }) => {
 
                 <Card
                     actions={[
-                        <Tooltip title={tooltip}>
-                            <a onClick={handleAddToCart}>
-                                <ShoppingCartOutlined className="text-danger" /> <br /> Add to
-                                Cart
+                        <Tooltip placement="top" title={tooltip}>
+                            <a onClick={handleAddToCart} disabled={product.quantity < 1}>
+                                <ShoppingCartOutlined className="text-danger" />
+                                <br />
+                                {product.quantity < 1 ? "Out of Stock" : "Add To Cart"}
                             </a>
                         </Tooltip>,
-                        <Link to="/">
+                        <a onClick={handleAddToWishlist}>
                             <HeartOutlined className="text-info" /> <br /> Add to Wishlist
-                        </Link>,
+                        </a>,
                         <RatingModal>
                             <StarRating
                                 name={_id}
